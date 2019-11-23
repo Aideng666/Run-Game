4daab3b245cfe1b236b85ddd99a32102ff65368a
Tyler <tyler.wong@ontariotechu.net>
2019-11-23 17:43:46 -0500

    V6.1

--- Rainbow Runner/Rainbow Runner/RainbowRunnerGame.cpp
+++ Rainbow Runner/Rainbow Runner/RainbowRunnerGame.cpp
@@ -71,7 +71,7 @@ void RainbowRunnerGame::InitScene(float windowWidth, float windowHeight)
 
 		std::string fileName = "GreyPlatformL.png";
 		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 256, 183);
-		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, -35.f, 100.f));
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(50.f, -35.f, 100.f));
 
 		unsigned int bitHolder = EntityIdentifier::SpriteBit() | EntityIdentifier::TransformBit();
 		ECS::SetUpIdentifier(entity, bitHolder, "Large Grey Platform");	
